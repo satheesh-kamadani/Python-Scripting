ed5f249ceb4a069af6bf4c54a2b2bd02c1b7c189
Kamadani, Satheesh <satheesh.kamadani@volvocars.com>
2023-09-01 05:31:41 +0200

    Automated Commit

--- list.py
+++ list.py
@@ -1,10 +1,19 @@
 fruites = ['mango', 'banana', 'grapes']
 print(fruites)
+fruites[1] = 'kiwi_fruite'
 fruites.append('kiwi')
 print(fruites)
 fruites.insert(0, 'apple')
 print(fruites)
 fruites.remove('grapes')
+print(fruites)
+fruites.append('cherries')
+print(fruites)
+
+removed_items = fruites.pop()
+print(removed_items)
+print(fruites)
+fruites.pop(0)
 print(fruites)
 
 for i in fruites:
